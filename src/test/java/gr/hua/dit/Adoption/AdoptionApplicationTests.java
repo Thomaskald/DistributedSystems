678b118dff4afd208ba25c96678b535c83f4d0b8
FOTIS21 <fotisxatz603@gmail.com>
2024-12-12 16:56:42 +0200

    Code

---
+++ src/test/java/gr/hua/dit/Adoption/AdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.Adoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
