bb7fd71bf17256f04169c0b0173c9cd5561cda87
FOTIS21 <fotisxatz603@gmail.com>
2025-01-01 19:39:04 +0200

    Connects to site, created passwordEncrypter, commented out some lines in Vet.Java

--- src/main/java/gr/hua/dit/Adoption/entities/Vet.java
+++ src/main/java/gr/hua/dit/Adoption/entities/Vet.java
@@ -1,30 +1,31 @@
 package gr.hua.dit.Adoption.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
 @Entity
-@Table( name = "vets")
-public class Vet extends User{
-
-    private long licenseNumber;
-
-    private List<String> checkedPets;
-
-    public Vet() {
-
-    }
-
-    public Vet(long licenseNumber, List<String> checkedPets) {
-        this.licenseNumber = licenseNumber;
-        this.checkedPets = checkedPets;
-    }
-
-    public long getLicenseNumber() {return licenseNumber;}
-    public void setLicenseNumber(long licenseNumber) {this.licenseNumber = licenseNumber;}
+public class Vet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+//
+//    private long licenseNumber;
+//
+//    private List<String> checkedPets;
+//
+//    public Vet() {
+//
+//    }
+//
+//    public Vet(long licenseNumber, List<String> checkedPets) {
+//        this.licenseNumber = licenseNumber;
+//        this.checkedPets = checkedPets;
+//    }
+//
+//    public long getLicenseNumber() {return licenseNumber;}
+//    public void setLicenseNumber(long licenseNumber) {this.licenseNumber = licenseNumber;}
 
 /*    public List<String> getCheckedPets() {return checkedPets;}
     public void setCheckedPets(List<String> checkedPets) {this.checkedPets = checkedPets;}*/
