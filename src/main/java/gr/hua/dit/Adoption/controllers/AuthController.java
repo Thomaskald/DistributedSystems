a975599b7066a53ee421ec46c41d699a76aa8b7f
Tomasito <thomaskald849@gmail.com>
2025-01-07 21:41:32 +0200

    Updated AuthController

--- src/main/java/gr/hua/dit/Adoption/controllers/AuthController.java
+++ src/main/java/gr/hua/dit/Adoption/controllers/AuthController.java
@@ -17,11 +17,15 @@ public class AuthController {
 
     @PostConstruct
     public void setup() {
-        Role role_user = new Role("ROLE_USER");
+        Role role_adopter = new Role("ROLE_ADOPTER");
         Role role_admin = new Role("ROLE_ADMIN");
+        Role role_vet = new Role("ROLE_VET");
+        Role role_shelter_admin = new Role("ROLE_SHELTER_ADMIN");
 
-        roleRepository.updateOrInsert(role_user);
+        roleRepository.updateOrInsert(role_adopter);
         roleRepository.updateOrInsert(role_admin);
+        roleRepository.updateOrInsert(role_vet);
+        roleRepository.updateOrInsert(role_shelter_admin);
     }
 
     @GetMapping("/login")
