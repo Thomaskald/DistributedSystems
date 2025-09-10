cbd686bf4aa75babe3117ec3929b16e365de74a9
FOTIS21 <fotisxatz603@gmail.com>
2025-09-10 21:22:37 +0300

    Added MockBean to AdoptionApplicationTests.java

--- src/test/java/gr/hua/dit/Adoption/AdoptionApplicationTests.java
+++ src/test/java/gr/hua/dit/Adoption/AdoptionApplicationTests.java
@@ -2,9 +2,14 @@ package gr.hua.dit.Adoption;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class AdoptionApplicationTests {
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
